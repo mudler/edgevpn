445937d1b7c9941787a00b4cb70edde9b9b3bbd7
Ettore Di Giacinto <mudler@sabayon.org>
2021-12-28 23:31:19 +0100

    :notebook: fixup LICENSE headers

--- pkg/edgevpn/interface_windows.go
+++ pkg/edgevpn/interface_windows.go
@@ -1,13 +1,28 @@
 //go:build windows
 // +build windows
 
+// Copyright © 2021 Ettore Di Giacinto <mudler@mocaccino.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/>.
+
 package edgevpn
 
 import (
-	"net"
-	"os/exec"
 	"fmt"
 	"log"
+	"net"
+	"os/exec"
 
 	"github.com/songgao/water"
 )
@@ -35,7 +50,7 @@ func (e *EdgeVPN) createInterface() (*water.Interface, error) {
 	}
 	network := net.IPNet{
 		IP:   ip,
-		Mask: net.IPv4Mask(0,0,0,0),
+		Mask: net.IPv4Mask(0, 0, 0, 0),
 	}
 	config := water.Config{
 		DeviceType: e.config.DeviceType,
