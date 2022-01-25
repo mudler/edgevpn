2e841e6c25ebd159ec9fbf9b1e61772cad25e4d0
Ettore Di Giacinto <mudler@sabayon.org>
2022-01-25 20:27:20 +0100

    :robot: Add codecov

--- .github/tests.sh
+++ .github/tests.sh
@@ -5,8 +5,9 @@ set -ex
 GO111MODULE=off go get github.com/onsi/ginkgo/ginkgo
 GO111MODULE=off go get github.com/onsi/gomega/...
 
-go test -race ./pkg/...
 
 ./edgevpn api &
 
-TEST_INSTANCE="http://localhost:8080" go test ./api/client
+export TEST_INSTANCE="http://localhost:8080"
+
+go test -coverprofile=coverage.txt -covermode=atomic -race ./pkg/... ./api/client
