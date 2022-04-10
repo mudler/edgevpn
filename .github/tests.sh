85cb21df56599bd8032e78323d4309d9f78ef012
Ettore Di Giacinto <mudler@c3os.io>
2022-04-10 14:15:45 +0200

    :bug: CI fixups

--- .github/tests.sh
+++ .github/tests.sh
@@ -2,14 +2,7 @@
 
 set -ex
 
-go get github.com/onsi/ginkgo/v2
-go get github.com/onsi/gomega/...
-
-go mod tidy
-
-go get github.com/onsi/ginkgo/v2/ginkgo/internal@v2.1.1
-
-go install github.com/onsi/ginkgo/v2/ginkgo
+go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo
 
 ./edgevpn api &
 
