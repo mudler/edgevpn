1b8592ff8ccc52e8f9158bc42eb310e1118a4559
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-12-08 17:52:32 +0000

    Revert ":arrow_up: Bump libp2p to v0.24.0"
    This fails all test still. Needs separate investigation
    
    This reverts commit 12671d4be22cb7312d9489cc761fedbb664c0c37.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -238,6 +238,9 @@ func (c Config) ToOpts(l *logger.Logger) ([]node.Option, []vpn.Option, error) {
 		}
 
 		staticRelays := c.Connection.StaticRelays
+		if len(staticRelays) == 0 {
+			staticRelays = autorelay.DefaultRelays
+		}
 		if c.Connection.AutoRelayDiscoveryInterval == 0 {
 			c.Connection.AutoRelayDiscoveryInterval = 5 * time.Minute
 		}
@@ -279,7 +282,7 @@ func (c Config) ToOpts(l *logger.Logger) ([]node.Option, []vpn.Option, error) {
 	}
 
 	if !c.Limit.Enable || runtime.GOOS == "darwin" {
-		libp2pOpts = append(libp2pOpts, libp2p.ResourceManager(&network.NullResourceManager{}))
+		libp2pOpts = append(libp2pOpts, libp2p.ResourceManager(network.NullResourceManager))
 	} else {
 		var limiter rcmgr.Limiter
 
